0181a3742e76947bb0013a551010e34cc04edf2a
Holiday <araragikyn@gmail.com>
2023-04-18 01:23:14 +0800

    some fix clients

--- RehearsalLibrary/Entities/StudioClient.cs
+++ RehearsalLibrary/Entities/StudioClient.cs
@@ -4,14 +4,8 @@ using RehearsalLibrary.Interfaces;
 namespace RehearsalLibrary.Entities;
 public class StudioClient : Client
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public ICollection<GroupEntry>? GroupEntries { get; set; }
-    public StudioUserTypes UserType { get; set; }
-    public ICollection<IndividualEntry>? IndividualEntries { get; set; }
-    public ICollection<VocalEntry>? VocalEntries { get; set; }
-    public ICollection<Comment>? Comments { get; set; }
-    public string PhoneNumber { get; set; }
+   
+   
     public string Email { get; set; }
     public DateTime RegistrationDate { get; set; }
 
--- RehearsalLibrary/Entities/StudioGuest.cs
+++ RehearsalLibrary/Entities/StudioGuest.cs
@@ -5,12 +5,6 @@ namespace RehearsalLibrary.Entities;
 
 public class StudioGuest : Client
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string PhoneNumber { get; set; }
-    public ICollection<GroupEntry> GroupEntries { get; set; }
-    public ICollection<IndividualEntry> IndividualEntries { get; set; }
-    public ICollection<VocalEntry> VocalEntries { get; set; }
-    public ICollection<Comment> Comments { get; set; }
+   
 }
 
