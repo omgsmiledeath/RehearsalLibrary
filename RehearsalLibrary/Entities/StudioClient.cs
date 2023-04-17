61570c07da25f1fe83ef85293288dceffb9bda81
kseniabelaevskaa <araragikyn@gmail.com>
2023-04-17 13:34:39 +0800

    StudioClient realisation interface

--- RehearsalLibrary/Entities/StudioClient.cs
+++ RehearsalLibrary/Entities/StudioClient.cs
@@ -11,5 +11,13 @@ public class StudioClient : IRehearsalClient,IUser
     public ICollection<IndividualEntry>? IndividualEntries { get; set; }
     public ICollection<VocalEntry>? VocalEntries { get; set; }
     public ICollection<IComment>? Comments { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    
+    public bool CheckPassword(string password)
+    {
+        throw new NotImplementedException();
+    }
 }
 
